643a102c939240320ef8ee7671cf8fe507dceb9c
cyrog <cyross@pm.me>
2025-01-21 20:09:59 +0100

    bg avec des lignes

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { Canvas } from '@threlte/core';
+    import SceneOne from '../lib/SceneOne.svelte';
+	import { AsciiRenderer } from '@threlte/extras';
+    //import ParticleFloor from '../lib/ParticleFloor.svelte';
+ </script>
+ 
+ <section class="canvas-wrapper fixed inset-0">
+    <Canvas>
+      <SceneOne />
+     <!-- <AsciiRenderer/> -->
+    </Canvas>
+  </section>
