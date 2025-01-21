2922737b58de03b85717975a076b1c6a0099a2dd
cyrog <cyross@pm.me>
2025-01-21 16:14:05 +0100

    big sunset big model

--- README.md
+++
@@ -1,2 +0,0 @@
-# shennanigans
-wip
---
+++ new_threlte/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Canvas } from '@threlte/core';
+    import SceneOne from '../lib/SceneOne.svelte';
+    import ParticleFloor from '../lib/ParticleFloor.svelte';
+ </script>
+ 
+ <section class="canvas-wrapper fixed inset-0">
+    <Canvas>
+      <SceneOne />
+    </Canvas>
+  </section>
